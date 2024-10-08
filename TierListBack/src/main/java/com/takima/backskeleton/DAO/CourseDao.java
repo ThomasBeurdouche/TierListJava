73c7e00160ec90e32cec719329b93c46b9ce0fad
thomas.B <thomas.beurdouche@epfedu.fr>
2024-10-08 15:18:02 +0200

    first commit

---
+++ TierListBack/src/main/java/com/takima/backskeleton/DAO/CourseDao.java
@@ -0,0 +1,9 @@
+package main.java.com.takima.backskeleton.DAO;
+
+import com.takima.backskeleton.models.Course;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface CourseDao extends JpaRepository<Course, Long> {
+}
---
+++ TierListBack/src/test/java/com/takima/backskeleton/BackSkeletonApplicationTests.java
@@ -0,0 +1,13 @@
+package test.java.com.takima.backskeleton;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackSkeletonApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
